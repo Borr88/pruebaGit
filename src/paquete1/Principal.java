432030ac42171030da80ea4d2a7e1cde4f751331
borr1 <borr1@Portatil-Boris>
2024-04-15 09:08:12 +0200

    tercer commit

--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.print("Prueba control de versiones con Git");
 		System.out.print("Paso 2 con commit");
+		System.out.print("paso 3 con commit");
 	}
 
 }
